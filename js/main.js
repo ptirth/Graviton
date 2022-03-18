d3b736461d49d8a22061251f050d76f6ef76c2b3
Tirth Patel <tirth.patel1996@outlook.com>
2022-03-18 01:11:38 -0500

    resolves open issues

--- js/main.js
+++ js/main.js
@@ -3,23 +3,5 @@
     "use strict";
 
     /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100')
-
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-
-    
-    
 
 })(jQuery);
